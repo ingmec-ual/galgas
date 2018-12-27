c4f0a2f771156e6999bd6f97278447aff22473ee
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-12-27 11:28:55 +0100

    Import Arduino firmware to Atmel Studio

---
+++ galgas_firmware/galgas_firmware/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 27/12/2018 11:21:02
+ * Author : jlblanco
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ galgas_firmware/galgas_firmware/firmware/Sketch.cpp
@@ -0,0 +1,43 @@
+﻿/*
+  Sketch.cpp - Main file for "Didactic Strain Gauges-LED System"
+  Copyright (c) 2018-2019 University of Almeria.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <Arduino.h>
+
+// LEDs: 
+constexpr int LED1_PIN = 9; // uC pin 13, PB1
+constexpr int LED2_PIN = 10; // uC pin 14, PB2
+
+void setup()
+{
+	pinMode(LED1_PIN, OUTPUT);
+	pinMode(LED2_PIN, OUTPUT);
+}
+
+void loop() 
+{
+	// put your main code here, to run repeatedly:
+	for (int pc = 0; pc <255; pc++)
+	{
+		analogWrite(LED1_PIN, pc);
+		analogWrite(LED2_PIN, pc);
+		delay(5);
+	}
+
+}
+
