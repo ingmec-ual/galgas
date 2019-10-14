d428e9a20eca0633f5950a4b12c4c03b85abed19
Laura1396 <lgalvarez1396@gmail.com>
2019-10-14 17:49:57 +0200

    elimino script antiguos

--- matlab/calculo_caso2.m
+++
@@ -1,7 +0,0 @@
-function [F]=calculo_caso2(N)
-% calculo del peso cuando la persona se encuentra en el caso 2
-% vector de axiles en N
-v=N(1);
-f=-(v/0.5); % valor de la fuerza en N
-F=f/9.807;  % peso de la persona en Kg
-end
