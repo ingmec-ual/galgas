1bb31bf6050b46325aaa8b879334dd691c5d9f32
Laura1396 <lgalvarez1396@gmail.com>
2019-10-16 18:03:45 +0200

    modifico y añado comentarios explicativos

--- matlab/pantalla.m
+++ matlab/pantalla.m
@@ -1,28 +1,7 @@
 function varargout = pantalla(varargin)
-%PANTALLA M-file for pantalla.fig
-%      PANTALLA, by itself, creates a new PANTALLA or raises the existing
-%      singleton*.
-%
-%      H = PANTALLA returns the handle to a new PANTALLA or the handle to
-%      the existing singleton*.
-%
-%      PANTALLA('Property','Value',...) creates a new PANTALLA using the
-%      given property value pairs. Unrecognized properties are passed via
-%      varargin to pantalla_OpeningFcn.  This calling syntax produces a
-%      warning when there is an existing singleton*.
-%
-%      PANTALLA('CALLBACK') and PANTALLA('CALLBACK',hObject,...) call the
-%      local function named CALLBACK in PANTALLA.M with the given input
-%      arguments.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help pantalla
-
-% Last Modified by GUIDE v2.5 15-Oct-2019 12:49:34
+% Interfaz del programa, muestra en pantalla la posicion en la que se
+% encuentra la persona, su peso, los valores de deformaci?n, axiles y
+% tensiones y las barras que estan sometidas a traccion y a compresion
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -41,32 +20,33 @@ if nargout
 else
     gui_mainfcn(gui_State, varargin{:});
 end
+end
 % End initialization code - DO NOT EDIT
 
 
 % --- Executes just before pantalla is made visible.
-function pantalla_OpeningFcn(hObject, eventdata, handles, varargin)
+function pantalla_OpeningFcn(hObject, ~, handles, varargin)
 
+% Muestra una grafica que representa la estructura del puente en el 
+% grafico axes3
 axes(handles.axes3);
-grafica_puente;
+grafica_puente; % Ejecuta script
 axis equal;
+% Muestra una imagen de la estructura del puente en el grafico axes1
 axes(handles.axes1);
 imshow('puente.jpg');
 axis equal;
+% Tabla de deformaciones, tensiones y axiles aparece vacia 
 datos=[];
 set(handles.tabla,'data',datos);
 % Choose default command line output for pantalla
 handles.output = hObject;
-
 % Update handles structure
 guidata(hObject, handles);
-
-% UIWAIT makes pantalla wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
+end
 
 % --- Outputs from this function are returned to the command line.
-function varargout = pantalla_OutputFcn(hObject, eventdata, handles)
+function varargout = pantalla_OutputFcn(~, ~, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -74,9 +54,15 @@ function varargout = pantalla_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+end
 
 % --- Executes on selection change in popupmenu.
-function popupmenu_Callback(hObject, eventdata, handles)
+function popupmenu_Callback(hObject, ~, handles)
+% Muestra en la tabla los valores de deformaciones, axiles y tensiones
+% correspondientes a cada barra segun se selecciona en el popupmenu
+% cg vector deformaciones medidas por las galgas en uE
+% t vector tensiones en MPa
+% N vector axiles en N
 global cg;
 global t;
 global N;
@@ -92,10 +78,18 @@ if(m==3)
     datos=N;    % Obtengo valores de axiles
 end
 set(handles.tabla,'data', datos);
+end
 
-
-% --- Executes on button press in run.
-function run_Callback(hObject, eventdata, handles)
+% --- Executes on button press in pesar.
+function pesar_Callback(~, ~, handles)
+% Calcula el peso de la persona que se encuentra sobre el puente
+% C matriz de casos 
+% cg vector deformaciones medidas por las galgas en uE
+% p indica el caso (posicion) en la que se encuentra la persona
+% t vector tensiones en MPa
+% N vector axiles en N
+% K matriz de constantes
+% F peso calculado
 global C;
 global cg;
 global p;
@@ -104,11 +98,11 @@ global N;
 global K;
 global F;
 
-C=casos;    
+C=casos;    % Ejecuto script
 cg=[-87.17293736, 116.2305831, 58.11529157,	-984.1815939, -492.0907969,	492.0907969, -492.0907969];
-% Calculo la posicion sobre el puente
+% Calculo la posicion sobre el puente 
 p=posicion(cg,C);
-% Muestro en pantalla la posicion en la que se encuentra el run
+% Muestro en pantalla la posicion en la que se encuentra la persona
 if(p==2)
     axes(handles.axes1);
     imshow('caso2.jpg');
@@ -141,25 +135,36 @@ K=constante;
 F=calculo_peso(N,K,p);
 
 set(handles.peso,'String',F);
-
+end
 
 % --- Executes on button press in reset.
-function reset_Callback(hObject, eventdata, handles)
+function reset_Callback(~, ~, handles)
+% Resetea la pantalla de la interfaz
+% Pone a 0 el valor del peso
 set(handles.peso,'String', 0);
+% Muestra imagen de la estructura del puente original, eliminando flechas 
+% que indican posicion 
 axes(handles.axes1);
 imshow('puente.jpg');
 axis equal;
+% Tabla de datos deformaciones, tensiones, axiles se vacia 
 datos=[];
 set(handles.tabla,'data',datos);
+% Grafica de la estructura del puente vuelve al original, sin indicar
+% barras sometidas a traccion o a compresion
 axes(handles.axes3);
 hold off;
 grafica_puente;
 axis equal;
-
-
+end
 
 % --- Executes when selected object is changed in uibuttongroup2.
-function uibuttongroup2_SelectionChangedFcn(hObject, eventdata, handles)
+function uibuttongroup2_SelectionChangedFcn(hObject, ~, handles)
+% Muestro en la grafica de la estructura del puente barras sometidas a
+% traccion o compresion marcandolas de color morado
+% cg vector deformaciones medidas por las galgas en uE
+% TC variable con valor "Traccion" o "Compresion" segun el boton que este
+% presionado
 global cg;
 
 TC=get(hObject,'String');
@@ -167,12 +172,19 @@ switch TC
     case 'Traccion'
         hold off
         axes(handles.axes3);
-        traccion(cg);
+        traccion(cg);   % Ejecuto script
         axis equal;
        
     case 'Compresion'
         hold off
         axes(handles.axes3);
-        compresion(cg);
+        compresion(cg); %Ejecuto script 
         axis equal;
+end
+end
+
+% --- Executes on button press in run.
+function run_Callback(hObject, eventdata, handles)
+uibuttongroup2_SelectionChangedFcn(hObject, eventdata, handles);
+popupmenu_Callback(hObject, eventdata, handles);
 end
