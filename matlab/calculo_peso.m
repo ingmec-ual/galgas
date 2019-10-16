5af9c5b5935a473734f2f99f5e2b7ae5ea034fe5
Laura1396 <lgalvarez1396@gmail.com>
2019-10-16 17:04:31 +0200

    simplifico calculo peso

--- matlab/calculo_peso.m
+++ matlab/calculo_peso.m
@@ -1,20 +1,8 @@
 function [F]=calculo_peso(N,K,p)
 %   Calculo del peso cuando la persona
 %   Solo se produce deformaci?n el?stica
-if(p==2)
-    f=N(1)/K(1,1);
-end
-if(p==3)
-    f=N(1)/K(2,1);
-end
-if(p==4)
-    f=N(1)/K(3,1);
-end
-if(p==5)
-    f=N(1)/K(4,1);
-end
-if(p==6)
-    f=N(1)/K(5,1);
-end
+
+f=median(N./K(p-1,:));
+
 F=f/9.81;
 end
