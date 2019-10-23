f81b1c514a858339eac426971d5bb51229683d64
Laura1396 <lgalvarez1396@gmail.com>
2019-10-23 20:08:20 +0200

    Solucion temporizador

--- matlab/pantalla.m
+++ matlab/pantalla.m
@@ -48,19 +48,6 @@ handles.timer = timer('Name','MyTimer',               ...
                       'ExecutionMode','fixedSpacing', ...
                       'TimerFcn',{@timerCallback,hObject}); 
                   
-% Muestra una grafica que representa la estructura del puente en el 
-% grafico grafica
-axes(handles.grafica);
-grafica_puente; % Ejecuta script
-axis equal;
-% Muestra una imagen de la estructura del puente en el grafico foto
-axes(handles.foto);
-imshow('puente.jpg');
-axis equal;
-% Tabla de deformaciones, tensiones y axiles aparece vacia 
-datos=[];
-set(handles.tabla,'data',datos);
-
 % Update handles structure
 guidata(hObject, handles);
 end
@@ -69,6 +56,21 @@ end
 % --- Outputs from this function are returned to the command line.
 function varargout = pantalla_OutputFcn(~, ~, handles)
 
+% Muestra una grafica que representa la estructura del puente en el 
+% grafico grafica
+axes(handles.grafica);
+grafica_puente; % Ejecuta script
+axis equal;
+
+% Muestra una imagen de la estructura del puente en el grafico foto
+axes(handles.foto);
+imshow('puente.jpg');
+axis equal;
+
+% Tabla de deformaciones, tensiones y axiles aparece vacia 
+datos=[];
+set(handles.tabla,'data',datos);
+
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 end
@@ -76,6 +78,7 @@ end
 
 % --- Se ejecuta cuando un objeto del tipo_esfuerzos cambia 
 function tipo_esfuerzos_SelectionChangedFcn(hObject, ~, handles)
+
 % Muestro en la grafica de la estructura del puente barras sometidas a
 % traccion o compresion marcandolas de color morado
 % cg vector deformaciones medidas por las galgas en uE
@@ -85,8 +88,9 @@ global cg;
 global TC;
 
 TC=get(hObject, 'String');
+
 switch TC
-    case 'Traccion'
+case 'Traccion'
         hold off
         axes(handles.grafica);
         traccion(cg);   % Ejecuto script
@@ -103,9 +107,9 @@ end
 
 % --- Funcion que se ejecuta al cambiar valores en popupmenu
 function popupmenu_Callback(hObject, ~, handles)
+
 % Muestra en la tabla los valores de deformaciones, axiles y tensiones
 % correspondientes a cada barra segun se selecciona en el popupmenu
-
 global datos;
 global m;
 
@@ -116,9 +120,76 @@ end
 
 
 % --- Funcion que se ejecuta al presionar boton medir
-function medir_Callback(hObject, ~, handles)
-% --- Calculo el peso que hay sobre el puente. --------------------------
-% Calcula el peso de la persona que se encuentra sobre el puente
+function medir_Callback(~, ~, handles)
+
+% Inicia el temporizador
+start(handles.timer);
+end
+
+
+% --- Funcion que se ejecuta al presionar boton resetear.
+function resetear_Callback(~, ~, handles)
+
+% Resetea la pantalla de la interfaz
+% Para el temporizador
+stop(handles.timer);
+
+% Pone a 0 el valor del peso
+texto=[];
+set(handles.peso,'String',texto);
+
+% Muestra imagen de la estructura del puente original, eliminando flechas 
+% que indican posicion 
+axes(handles.foto);
+imshow('puente.jpg');
+axis equal;
+
+% Tabla de datos deformaciones, tensiones, axiles se vacia 
+datos=[];
+set(handles.tabla,'data',datos);
+
+% Grafica de la estructura del puente vuelve al original, sin indicar
+% barras sometidas a traccion o a compresion
+axes(handles.grafica);
+hold off;
+grafica_puente;
+axis equal;
+end
+
+%------------------------------------------------------------------------
+
+% ---MODULOS---
+
+% --- Funcion que se ejecuta al llamar al popupmenu
+function ejecuto_popupmenu
+
+% Muestra en la tabla los valores de deformaciones, axiles y tensiones
+% correspondientes a cada barra segun se selecciona en el popupmenu
+% cg vector deformaciones medidas por las galgas en uE
+% t vector tensiones en MPa
+% N vector axiles en N
+% datos valores que se insertan en la tabla
+global m;
+global cg;
+global t;
+global N;
+global datos;
+
+if(m==1)
+    datos=cg;   % Obtengo valores de deformacion 
+end
+if(m==2)
+    datos=t;    % Obtengo valores de tension
+end
+if(m==3)
+    datos=N;    % Obtengo valores de axiles
+end
+end
+
+
+%--- Funcion que se repite mientras el temporizador esta en marcha
+ function timerCallback(~, ~, hFigure)
+ 
 % C matriz de casos 
 % cg vector deformaciones medidas por las galgas en uE
 % p indica el caso (posicion) en la que se encuentra la persona
@@ -126,6 +197,10 @@ function medir_Callback(hObject, ~, handles)
 % N vector axiles en N
 % K matriz de constantes
 % F peso calculado
+% datos valores que se insertan en la tabla
+% TC variable con valor "Traccion" o "Compresion"
+% texto variable contiene texto con valor peso que se impreme en pantalla
+handles = guidata(hFigure);
 global C;
 global cg;
 global p;
@@ -134,6 +209,11 @@ global N;
 global K;
 global F;
 global datos;
+global TC;
+global texto;
+
+% --- Calculo el peso que hay sobre el puente. --------------------------
+
 C=casos;    % Ejecuto script
 cg=[-87.17293736, 116.2305831, 58.11529157,	-984.1815939, -492.0907969,	492.0907969, -492.0907969];
 % Calculo la posicion sobre el puente 
@@ -170,104 +250,57 @@ t=calculo_tension(cg);
 N=calculo_axiles(t);
 K=constante;
 F=calculo_peso(N,K,p);
+
+% --- Muestro valores actuales del peso---------------------------------
 texto=sprintf('Peso: %5.1f Kg',F);
 set(handles.peso,'String',texto);
-% ----------------------------------------------------------------------
-% Muestro los valores actuales del popupmenu 
-%ejecuto_popupmenu;
-%set(handles.tabla,'data', datos);
-% Muestro los valores actuales de tipo_esfuerzos
-%switch TC
-%    case 'Traccion'
-%        hold off
-%        axes(handles.grafica);
-%        traccion(cg);   % Ejecuto script
-%        axis equal;
+
+% --- Muestro los valores actuales del popupmenu------------------------
+ejecuto_popupmenu;
+set(handles.tabla,'data', datos);
+
+% --- Muestro los valores actuales de tipo_esfuerzos--------------------
+switch TC
+    case 'Traccion'
+        hold off
+        axes(handles.grafica);
+        traccion(cg);   % Ejecuto script
+        axis equal;
        
-%    case 'Compresion'
-%        hold off
-%        axes(handles.grafica);
-%        compresion(cg); % Ejecuto script 
-%        axis equal;
-%end
-% Inicio el temporizador
-start(handles.timer);
-set(handles.tabla,'data', datos);
+    case 'Compresion'
+        hold off
+        axes(handles.grafica);
+        compresion(cg); % Ejecuto script 
+        axis equal;
 end
 
-
-% --- Funcion que se ejecuta al presionar boton resetear.
-function resetear_Callback(~, ~, handles)
-% Resetea la pantalla de la interfaz
-% Pone a 0 el valor del peso
-stop(handles.timer);
-texto=[];
-set(handles.peso,'String',texto);
-% Muestra imagen de la estructura del puente original, eliminando flechas 
-% que indican posicion 
-axes(handles.foto);
-imshow('puente.jpg');
-axis equal;
-% Tabla de datos deformaciones, tensiones, axiles se vacia 
-datos=[];
-set(handles.tabla,'data',datos);
-% Grafica de la estructura del puente vuelve al original, sin indicar
-% barras sometidas a traccion o a compresion
-axes(handles.grafica);
-hold off;
-grafica_puente;
-axis equal;
-end
-
-%------------------------------------------------------------------------
-% MODULOS
-
-% --- Funcion que se ejecuta al llamar al popupmenu
-function ejecuto_popupmenu
-% Muestra en la tabla los valores de deformaciones, axiles y tensiones
-% correspondientes a cada barra segun se selecciona en el popupmenu
-% cg vector deformaciones medidas por las galgas en uE
-% t vector tensiones en MPa
-% N vector axiles en N
-global m;
-global cg;
-global t;
-global N;
-global datos;
-
-if(m==1)
-    datos=cg;   % Obtengo valores de deformacion 
-end
-if(m==2)
-    datos=t;    % Obtengo valores de tension
-end
-if(m==3)
-    datos=N;    % Obtengo valores de axiles
-end
-
-end
-
-%--- Funcion que se repite mientras el temporizador esta en marcha
- function timerCallback(hTimer, eventdata, hFigure)
-handles = guidata(hFigure);
-global m;
-global cg;
-global t;
-global N;
-global datos;
-
-if(m==1)
-    datos=cg;   % Obtengo valores de deformacion 
-end
-if(m==2)
-    datos=t;    % Obtengo valores de tension
-end
-if(m==3)
-    datos=N;    % Obtengo valores de axiles
-end
-
-
-set(handles.tabla,'data', datos);
-
- guidata(hFigure,handles);
+guidata(hFigure,handles);
  end
+
+ 
+ % --- Executes during object creation, after setting all properties.
+ function peso_CreateFcn(hObject, ~, ~)
+ % hObject    handle to edit1 (see GCBO)
+ % eventdata  reserved - to be defined in a future version of MATLAB
+ % handles    empty - handles not created until after all CreateFcns called
+
+ % Hint: edit controls usually have a white background on Windows.
+ %       See ISPC and COMPUTER.
+ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+     set(hObject,'BackgroundColor','white');
+ end
+ end
+
+ 
+ % --- Executes during object creation, after setting all properties.
+function popupmenu_CreateFcn(hObject, ~, ~)
+% hObject    handle to popupmenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
